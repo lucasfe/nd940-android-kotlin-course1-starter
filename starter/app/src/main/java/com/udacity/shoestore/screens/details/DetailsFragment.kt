28bb0c6c09e2614dbbef501227208897f534d3a7
Lucas Ferreira <lucasfe@gmail.com>
2021-03-29 22:05:37 -0300

    Adding screens and navigation

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/details/DetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.udacity.shoestore.screens.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.DetailsFragmentBinding
+
+class DetailsFragment : Fragment() {
+
+private lateinit var binding : DetailsFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.details_fragment, container, false)
+        return binding.root
+    }
+}
